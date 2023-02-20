12eba141104f76323e0f2e52972b2665a2a1b9f6
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-02-20 14:53:11 +0300

    fix: some bugs fixed and scroll added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ const days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
 let date = new Date();
 let now = date.getDay();
 
+
+//test
+
 const dateParagraph = document.querySelector('.subheader__day-of-week');
 const timeParagraph = document.querySelector('.subheader__time');
 
 dateParagraph.textContent = days[now];
 timeParagraph.textContent = `${date.getHours()}:${ date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
-console.log(days[now]);
 
 /*
 
